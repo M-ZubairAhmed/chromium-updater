39496d12a80f5dffc32b540ecc5fbf36f1ebdc74
ZA <ahmedzubair216@gmail.com>
2018-02-21 12:28:26 +0530

    added progress lib

--- src/download.js
+++ src/download.js
@@ -1,34 +1,37 @@
 const https = require('https')
-
-const displayProgress = () => {
-  console.log('\033[2J')
-  const progress = Math.floor(Math.random() * 2 + 1)
-  if (progress % 2 === 0) {
-    console.log('Downloading........')
-  } else {
-    console.log('Downloading..')
-  }
-}
+const logUpdate = require('log-update')
 
 exports.default = url => {
   https
     .get(url, res => {
       const { statusCode } = res
-      const totalSize = parseInt(res.headers['content-length'])
+      const contentType = res.headers['content-type']
+      let downloadCounter = 0
+      const downloadFrames = ['----', '=---', '-=--', '--=-', '---=']
+
+      if (statusCode !== 200) {
+        throw new Error(`Request failed due to response code ${statusCode}`)
+      } else if (!/^application\/zip/.test(contentType)) {
+        throw new Error(`Expected a zip file, but recieved ${contentType}`)
+      }
 
       res.on('data', stream => {
-        displayProgress()
+        const downloadFrame =
+          downloadFrames[
+            (downloadCounter = ++downloadCounter % downloadFrames.length)
+          ]
+        logUpdate(`Downloading ${downloadFrame}`)
       })
 
       res.on('end', () => {
         try {
-          console.log('Download complete')
-        } catch (e) {
-          throw new Error(e)
+          console.log('Downloading completd')
+        } catch (err) {
+          throw new Error(`Download stopped abruptly: ${err}`)
         }
       })
     })
-    .on('error', e => {
-      console.error(e)
+    .on('error', err => {
+      console.error(err)
     })
 }
