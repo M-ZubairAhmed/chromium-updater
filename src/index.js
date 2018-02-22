6d3f07384b56cd3b35ff74d57983e6c5ec63f358
ZA <ahmedzubair216@gmail.com>
2018-02-22 16:34:10 +0530

    dependency on commander removed

--- src/index.js
+++ src/index.js
@@ -2,22 +2,9 @@ const precheck = require('./prechecks').default
 const verify = require('./prechecks').verify
 const download = require('./actions').download
 const unzip = require('./actions').unzip
-const commands = require('commander')
 const LATEST_VERSION_URL = require('./utils').LATEST_VERSION_URL
 
-commands
-  .version(process.env.npm_package_version)
-  .option('-d, --dir', 'Add directory of your chromium downloads')
-  .parse(process.argv)
-
-let chromiumDirectory
-if (commands.dir) {
-  chromiumDirectory = commands.dir
-} else {
-  chromiumDirectory = __dirname
-}
-
-precheck(chromiumDirectory, LATEST_VERSION_URL)
+precheck(__dirname, LATEST_VERSION_URL)
   .then(checkedInfo => {
     console.log('Checking installed Chromium/s in', checkedInfo.directory)
     return verify(checkedInfo)
